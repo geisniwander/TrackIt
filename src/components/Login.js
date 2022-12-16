c9541df2004c0e75338072c768e90a5bdd74b834
geisniwander <geisniwander@gmail.com>
2022-12-16 12:58:37 -0300

    API registration and login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,32 @@
 import logo from "../assets/logo.png";
 import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState("");
+  const navigate = useNavigate();
+  function login(e){
+    e.preventDefault();
+    const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login", {email: email.toString(), password: password.toString()});
+    promise.then((r) =>{setToken(r.data.token); navigate("/hoje")});
+    promise.catch((err)=> console.log(err));
+  }
+
   return (
     <ContainerLogin>
       <img alt="logo" src={logo} />
-      <Input type="email" placeholder="email" />
-      <Input type="password" placeholder="senha" />
-      <Button type="submit">Entrar</Button>
-      <p>Não tem uma conta? Cadastre-se!</p>
+      <Form onSubmit={login}>
+        <Input type="email" placeholder="email" value={email} onChange={(e)=> setEmail(e.target.value)} required />
+        <Input type="password" placeholder="senha" value={password} onChange={(e)=> setPassword(e.target.value)} required />
+        <Button type="submit">Entrar</Button>
+      </Form>
+      <Link to="/cadastro">
+        <p>Não tem uma conta? Cadastre-se!</p>
+      </Link>
     </ContainerLogin>
   );
 }
@@ -36,9 +54,18 @@ const ContainerLogin = styled.div`
     margin-top: 8%;
   }
 `;
-const Input = styled.input`
+
+const Form = styled.form`
   width: 80%;
-  height: 5%;
+  height: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  width: 98%;
+  height: 45px;
   margin: 2%;
   background: #ffffff;
   border: 1px solid #d5d5d5;
@@ -55,8 +82,8 @@ const Input = styled.input`
 `;
 
 const Button = styled.button`
-  width: 81%;
-  height: 5%;
+  width: 99%;
+  height: 45px;
   margin-top: 2%;
   background: #52b6ff;
   border-radius: 5px;
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,16 +1,67 @@
 import logo from "../assets/logo.png";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [image, setImage] = useState("");
+
+  function register(e) {
+    e.preventDefault();
+    const promise = axios.post(
+      "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up",
+      {
+        email: email.toString(),
+        name: name.toString(),
+        image: image.toString(),
+        password: password.toString(),
+      }
+    );
+    promise.then(() => console.log("Deu certo"));
+    promise.catch((err) => console.log(err));
+  }
+
   return (
     <ContainerSignUp>
       <img alt="logo" src={logo} />
-      <Input type="email" placeholder="email" />
-      <Input type="password" placeholder="senha" />
-      <Input type="text" placeholder="nome" />
-      <Input type="url" placeholder="foto" />
-      <Button type="submit">Cadastrar</Button>
-      <p>Já tem uma conta? Faça login!</p>
+      <Form onSubmit={register}>
+        <Input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <Input
+          type="password"
+          placeholder="senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <Input
+          type="text"
+          placeholder="nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <Input
+          type="url"
+          placeholder="foto"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+          required
+        />
+        <Button type="submit">Cadastrar</Button>
+      </Form>
+      <Link to="/">
+        <p>Já tem uma conta? Faça login!</p>
+      </Link>
     </ContainerSignUp>
   );
 }
@@ -39,9 +90,17 @@ const ContainerSignUp = styled.div`
   }
 `;
 
-const Input = styled.input`
+const Form = styled.form`
   width: 80%;
-  height: 5%;
+  height: 35%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  width: 98%;
+  height: 45px;
   margin: 2%;
   background: #ffffff;
   border: 1px solid #d5d5d5;
@@ -58,8 +117,8 @@ const Input = styled.input`
 `;
 
 const Button = styled.button`
-  width: 81%;
-  height: 5%;
+  width: 99%;
+  height: 45px;
   margin-top: 2%;
   background: #52b6ff;
   border-radius: 5px;
